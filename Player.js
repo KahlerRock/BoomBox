fc684cf519e4df91fb5ec5c5b85d0669f6232eef
KahlerRockwell <Rockwell1023@gmail.com>
2020-04-21 16:22:51 -0400

    adds Server.html

--- Player.js
+++ Player.js
@@ -1,8 +1,13 @@
-window.onload = function () {
+
+window.onload = function (){
+  document.getElementById("loaded").innerHTML = "loaded";
+}
+
+function playMusic () {
     var player = new window.Audio('youtube.com/watch?v=s-mOy8VUEBk')
     player.preload = 'metadata'
     player.play()
     player.controls = true
     document.body.appendChild(player)
   }
-  \
+  
