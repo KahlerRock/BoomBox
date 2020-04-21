1dae5bbbf43e7fbf78fa1fac3e4ea069d70792ca
KahlerRockwell <Rockwell1023@gmail.com>
2020-04-21 17:09:25 -0400

    adds iframe

--- Player.js
+++ Player.js
@@ -4,10 +4,10 @@ window.onload = function (){
 }
 
 function playMusic () {
-    var player = new window.Audio('youtube.com/watch?v=s-mOy8VUEBk')
-    player.preload = 'metadata'
-    player.play()
-    player.controls = true
-    document.body.appendChild(player)
-  }
-  
+  console.log("playMusic");
+  var player = new window.Audio('https://www.youtube.com/watch?v=p4OHVJxd2FI')
+  player.preload = 'metadata'
+  player.play()
+  player.controls = true
+  document.body.appendChild(player)
+}
