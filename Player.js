bad111421b730a550c2e5a5bd2dad62fbc094745
KahlerRockwell <Rockwell1023@gmail.com>
2020-04-23 21:25:38 -0400

    adds search function

--- Player.js
+++ Player.js
@@ -1,13 +1,20 @@
 
 window.onload = function (){
-  document.getElementById("loaded").innerHTML = "loaded";
+  document.getElementById("player").innerHTML = "Player.js: loaded";
 }
 
 function playMusic () {
   console.log("playMusic");
-  var player = new window.Audio('https://www.youtube.com/watch?v=p4OHVJxd2FI')
+  var player = new window.Audio('https://www.youtube.com/watch?v=30PvYhVKNGgs')
   player.preload = 'metadata'
   player.play()
   player.controls = true
   document.body.appendChild(player)
+}
+
+function search(){
+  var s = document.getElementById("searchBar").value;
+  var para = document.createElement("P");
+  para.innerHTML = s;
+  document.getElementById("songList").appendChild(para);
 }
